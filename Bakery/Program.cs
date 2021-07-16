ae59df68afe1efe19d21a6d64722bfee93e3054e
Erika Debelis <erika.debelis@gmail.com>
2021-07-16 13:40:16 -0700

    add try/catch to branching in program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,16 +11,36 @@ namespace Bakery
     //   Console.WriteLine("Welcome to Pierre's Bakery Web Application");
     //   Console.WriteLine("__________________________________________");
     //   Console.WriteLine("Would you like to order bread or pastries? Type 'bread' for bread and 'pastries' for pastries.");
-    //    string bakeryChoice = Console.ReadLine();
+    //    string bakeryChoice = Console.ReadLine().ToUpper();
     //    if (bakeryChoice == "bread")
     //      {
+    //        try
+    //        {
+    //        Console.WriteLine("We're currently running an AMAZING bread special! Buy 2 loaves- get one FREE")
+    //        Console.WriteLine("How many loaves would you like to purchase?")
+    //        inputtedBreadOrder= Console.ReadLine();
     //        int totalBreadCost = Bread.BreadTotal(inputtedBreadOrder);
     //        Console.WriteLine(totalBreadCost);
+    //        }
+    //        catch
+    //        {
+    //        Console.WriteLine("Invalid input: enter a positive numeric integer to proceed.")
+    //        }
     //      }
     //      else if (bakeryChoice = "pastries")
     //      {
+    //        try
+    //        {
+    //        Console.WriteLine("We're currently running an PHENOMENAL pastry special! Buy 1 pastry for $2 or 3 pastries for $5!")
+    //        Console.WriteLine("How many pastries would you like to purchase?")
+    //        inputtedPastryOrder= Console.ReadLine();
     //        int totalPastryCost = Pastry.PastryTotal(inputtedPastryOrder);
     //        Console.WriteLine(totalPastryCost);
+    //        }
+    //        catch
+    //        {
+    //        Console.WriteLine("Invalid input: enter a positive numeric integer to proceed.")
+    //        }
     //      }
     //      else
     //      {
